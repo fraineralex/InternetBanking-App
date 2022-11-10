94c0b850559d3cb1e6086687e06643e009f5475f
LuisMorla <Luis0045@outlook.es>
2022-11-09 19:28:06 -0500

    Update Persistence

---
+++ InternetBanking.Infrastructure.Persistense/Repository/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Infrastructure.Persistense.Repository
+{
+    public class GenericRepository
+    {
+    }
+}
