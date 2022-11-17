6664fa446a253ea2daa27e46e2e7fc1060ef661c
Frainer <fraineralex2001@gmail.com>
2022-11-17 15:17:44 -0400

    [FIX] fix bug in the service registration file on the 'InternetBanking.Infrastructure.Persistence' layer

--- InternetBanking.Infrastructure.Persistense/ServiceRegistration.cs
+++ InternetBanking.Infrastructure.Persistense/ServiceRegistration.cs
@@ -18,24 +18,24 @@ namespace InternetBanking.Infrastructure.Persistence
     {
         public static void AddPersistenceInfrastructure(this IServiceCollection services,IConfiguration configuration)
         {
-            //#region Contexts
-            //if (configuration.GetValue<bool>("UseInMemoryDatabase"))
-            //{
-            //    services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
-            //}
-            //else
-            //{
-            //    services.AddDbContext<ApplicationContext>(options =>
-            //    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-            //    m=> m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
-            //}
-            //#endregion
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+            }
+            #endregion
 
-            //#region Repositories
+            #region Repositories
             //services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             //services.AddTransient<IProductRepository, ProductRepository>();
-            //services.AddTransient<ICategoryRepository, CategoryRepository>();  
-            //#endregion
+            //services.AddTransient<ICategoryRepository, CategoryRepository>();
+            #endregion
         }
     }
 }
